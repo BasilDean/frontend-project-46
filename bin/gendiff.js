4eac14e6052e6733f5d715c55b1dc6d4d9f3bf5c
Basil <duvanov@ivit.pro>
2024-06-09 11:03:16 +0400

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
 
 program.parse();
 
 const options = program.opts();
+
 
