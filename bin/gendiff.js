31337f3bfbbc449672c033ab170255c2f821eb52
Basil <duvanov@ivit.pro>
2024-06-09 15:21:50 +0400

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import parseFile from "../src/parseFile.js";
 const program = new Command();
 
 program
@@ -7,11 +8,15 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', parseFile(filepath1));
+    console.log('filepath2:', parseFile(filepath2));
+  });
 
 program.parse();
 
-const options = program.opts();
 
 
---
+++ src/parseFile.js
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs';
+import { cwd } from 'node:process';
+import path from 'node:path';
+
+/**
+ * @param {string | Buffer | URL | number} file
+ */
+const parseFile = (file) => {
+  // return path.resolve(cwd(), file)
+ const result = fs.readFileSync(path.resolve(cwd(), file));
+ if (file.endsWith('.json')) {
+   return JSON.parse(result);
+ }
+ return result;
+};
+
+export default parseFile;
